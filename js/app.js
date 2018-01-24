b29095b12448617425d580fe1ec21e7578722bcd
Sara Rykov <sara.rykov@gmail.com>
2018-01-24 10:20:22 -0500

    refactor: Refactor code to use MVO design pattern

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
-document.addEventListener('DOMContentLoaded', initialize);
-
-class Model {
-	constructor(){
-		this.allCats = new Map();
-	}
-	addCat(cat){
+/*
+ * Model contains all of the data for the application.
+ * Contains a map of all of our cats.
+ */
+var model = {
+	allCats: new Map(),
+	selectedCatId: '',
+	addCat: function(cat){
 		this.allCats.set(cat.id, cat);
-	}
-	getCat(catId){
+	},
+	getCat: function(catId){
 		return this.allCats.get(catId);
 	}
 }
 
+/*
+ * Represents a single cat.
+ */
 class Cat{
-	constructor(img, name, id, selected = false){
+	constructor(img, name, id){
 		this.img = img;
 		this.name = name;
 		this.id = id;
-		this.selected = selected;
 		this.clicks = 0;
 	}
 	resetClickCounter(){
@@ -27,120 +30,116 @@ class Cat{
 		this.clicks++;
 	}
 
-	makeChosenOne(){
+}
+//-------------------------------------------------------------
+
+var octopus = {
+	initializeApplication: function(){
+		this.initializeModel();
+		this.initializeView();
+	},
+
+	initializeModel: function(){
+		//add Furlicity and make her the chosen one
+		let furlicity = new Cat('img/furlicity.jpg', 'Furlicity', 'furlicity');
+		model.addCat(furlicity);
+		model.selectedCatId = furlicity.id;
+
+		//add Mackerel
+		let mackerel = new Cat('img/mackerel.jpg', 'Mackerel', 'mackerel');
+		model.addCat(mackerel);
+
+		//add Snowball
+		let snowball = new Cat('img/snowball.jpg', 'Snowball', 'snowball');
+		model.addCat(snowball);
+
+		//add Buddy
+		let buddy = new Cat('img/buddy.jpg', 'Buddy', 'buddy');
+		model.addCat(buddy);
+
+		//add The Twins
+		let twins = new Cat('img/thetwins.jpg', 'The Twins', 'twins');
+		model.addCat(twins);
+	},
+
+	initializeView: function(){
+		view.initializeCatSelectionList(model.selectedCatId);
+		this.selectCat(model.selectedCatId);
+	},
+
+	selectCat: function(selectedCatId){
+		model.selectedCatId = selectedCatId;
+		let selectedCat = model.getCat(selectedCatId);
+		view.loadCat(selectedCat);
+	},
+
+	clickCat: function(clickedCatId){
+		let clickedCat = model.getCat(clickedCatId);
+		clickedCat.clicks++;
+		view.updateClickCount(clickedCat);
+	}
+}
+
+//---------------------------------------------------------------
+var view = {
+	initializeCatSelectionList: function(selectedCatId){
+		const container = document.querySelector('.cat-list');
+
+		for([catName, cat] of model.allCats){
+			const catOption = document.createElement('div');
+			catOption.classList.add('cat-option');
+			catOption.setAttribute('id', catName.toLowerCase());
+			catOption.textContent = cat.name;
+
+			if(cat.id == selectedCatId){
+				catOption.classList.add('selected');
+			}
+			catOption.addEventListener('click', this.handleCatOptionClick);
+			container.appendChild(catOption);
+		}
+	},
+
+	handleCatOptionClick: function(event){
+		const selectedOption = event.target.closest('.cat-option');
+		const allOptions = document.querySelectorAll('.cat-option');
+		for(option of allOptions){
+			option.classList.remove('selected');
+		}
+		selectedOption.classList.add('selected');
+		const selectedCatId = selectedOption.id;
+		octopus.selectCat(selectedCatId);
+	},
+
+	handleCatImageClick: function(event){
+
+		//only handle click events on cat-pics
+		if(event.target.nodeName.toLowerCase() == 'img'){
+			//figure out which cat & register click
+			const catId = event.target.getAttribute('data-key');
+			octopus.clickCat(catId);
+		}
+	},
+
+	updateClickCount: function(cat){
+		//update page with new click count
+		const counter = document.querySelector('.clicks');
+		counter.innerText = cat.clicks;
+	},
+
+	loadCat: function(selectedCat){
 		const catName = document.querySelector('.cat-name');
-		catName.innerText = this.name;
+		catName.innerText = selectedCat.name;
 
 		const catImage = document.querySelector('.cat-pic');
-		catImage.setAttribute('src', this.img);
-		catImage.setAttribute('data-key', this.id);
+		catImage.setAttribute('src', selectedCat.img);
+		catImage.setAttribute('data-key', selectedCat.id);
 
 		const clicks = document.querySelector('.clicks');
 		clicks.classList.add('clicks');
-		clicks.innerText = this.clicks;
+		clicks.innerText = selectedCat.clicks;
 
-		catImage.addEventListener('click', handleCatImageClick);
+		catImage.addEventListener('click', this.handleCatImageClick);
 	}
 }
 
-let model;
-
-function initialize(){
-
-	initializeCats();
-	initializeOptions();
-
-}
-
-function initializeOptions(){
-	const container = document.querySelector('.cat-list');
-
-	for([catName, cat] of model.allCats){
-		const catOption = document.createElement('div');
-		catOption.classList.add('cat-option');
-		catOption.setAttribute('id', catName.toLowerCase());
-		catOption.textContent = cat.name;
-
-		if(cat.selected){
-			catOption.classList.add('selected');
-			cat.makeChosenOne();
-		}
-
-		catOption.addEventListener('click', handleCatOptionClick);
-		container.appendChild(catOption);
-	}
-
-}
-
-function initializeCats(){
-	 model = new Model();
-
-	//add Furlicity
-	let furlicity = new Cat('img/furlicity.jpg', 'Furlicity', 'furlicity', true);
-	model.addCat(furlicity);
-
-	//add Mackerel
-	let mackerel = new Cat('img/mackerel.jpg', 'Mackerel', 'mackerel');
-	model.addCat(mackerel);
-
-	//add Snowball
-	let snowball = new Cat('img/snowball.jpg', 'Snowball', 'snowball');
-	model.addCat(snowball);
-
-	//add Buddy
-	let buddy = new Cat('img/buddy.jpg', 'Buddy', 'buddy');
-	model.addCat(buddy);
-
-	//add The Twins
-	let twins = new Cat('img/thetwins.jpg', 'The Twins', 'twins');
-	model.addCat(twins);
-}
-
-function initializeList(){
-
-	const catList = document.querySelector('.cat-list');
-
-	for([catName, cat] of model.allCats){
-		const catContainerDiv = cat.getHtml();
-		container.appendChild(catContainerDiv);
-	}
-}
-
-/*
- * Handle click event on a cat pic.
- *
- */
-function handleCatImageClick(event){
-
-	//only handle click events on cat-pics
-	if(event.target.nodeName.toLowerCase() == 'img'){
-		const catContainer = event.target.closest('.cat-container');
-		const catId = event.target.getAttribute('data-key');
-
-		const cat = model.getCat(catId);
-		cat.addClick();
-
-		const counter = catContainer.querySelector('.clicks');
-		counter.innerText = cat.clicks;
-
-	}
-}
-
-/*
- * Handle click event on a cat option.
- *
- */
-function handleCatOptionClick(event){
-	const selectedOption = event.target.closest('.cat-option');
-
-	const options = document.querySelectorAll('.cat-option');
-	for(option of options){
-		option.classList.remove('selected');
-	}
-	selectedOption.classList.add('selected');
-
-	const catId = selectedOption.id;
-	const cat = model.getCat(catId);
-
-	cat.makeChosenOne();
-}
+document.addEventListener('DOMContentLoaded', octopus.initializeApplication());
